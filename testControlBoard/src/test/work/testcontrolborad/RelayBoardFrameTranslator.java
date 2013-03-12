475980654831a967c8cf2870136230321b7d7ef3
pdm <ppppdm@gmail.com>
2013-03-12 14:11:58 +0800

    add file RelayBoardFrameTranslator.java

---
+++ testControlBoard/src/test/work/testcontrolborad/RelayBoardFrameTranslator.java
@@ -0,0 +1,26 @@
+package test.work.testcontrolborad;
+
+public class RelayBoardFrameTranslator {
+	
+	//Constant variables
+	public static int LIGHT0 = 0;
+	public static int LIGHT1 = 1;
+	public static int LIGHT2 = 2;
+	public static int LIGHT3 = 3;
+	public static int LIGHT4 = 4;
+	public static int LIGHT5 = 5;
+	public static int LIGHT6 = 6;
+	public static int LIGHT7 = 7;
+	
+	public static int SWITCH0 = 0;
+	public static int SWITCH1 = 1;
+	public static int SWITCH2 = 2;
+	public static int SWITCH3 = 3;
+	
+	// Command
+	public static int OPEN_ONE_LIGHT = 0;
+	public static int CLOSE_ONE_LIGHT = 1;
+	public static int OPEN_ALL_LIGHT = 2;
+	public static int CLOSE_ALL_LIGHT = 3;
+	public static int READ_SWITCH_STATUS = 4;
+}
