a70ef41bd0fa2ea2bfe9a9ba6ba376c6376248db
kissy <ppppdm@gmail.com>
2013-03-21 18:34:31 +0800

    test bootCompleteReceiver, can't receive broadcast, on onda tablet

--- testControlBoard/src/test/work/testcontrolborad/BootCompletedReceiver.java
+++ testControlBoard/src/test/work/testcontrolborad/BootCompletedReceiver.java
@@ -3,12 +3,15 @@ package test.work.testcontrolborad;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
+	String tag = "bootcomplete";
 	@Override
 	public void onReceive(Context context, Intent arg1) {
 		// TODO Auto-generated method stub
+		Log.v(tag,"onRecive");
 		//activity
 		Intent acIntent = new Intent(context, TestControlBoradActivity.class); 
 		acIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  //注意，必须添加这个标记，否则启动会失败 
