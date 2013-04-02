72e309de376ce5337d4f3db216eda32ff836b967
pdm <ppppdm@gmail.com>
2013-04-02 19:08:34 +0800

    add tag to strategy setting Button xml file And added strings.xml resources file

--- SmartTerminal/gen/com/dorm/smartterminal/R.java
+++ SmartTerminal/gen/com/dorm/smartterminal/R.java
@@ -111,17 +111,41 @@ public final class R {
         public static final int st__settings__security_setting__strategy_setting=0x7f030008;
     }
     public static final class string {
-        public static final int action_settings=0x7f070001;
-        public static final int app_name=0x7f070000;
-        public static final int title_activity_activity1=0x7f070002;
-        public static final int title_activity_activity2=0x7f070003;
-        public static final int title_activity_activity3=0x7f070004;
-        public static final int title_activity_local_setting=0x7f070007;
-        public static final int title_activity_security_setting=0x7f070008;
-        public static final int title_activity_sensor_setting=0x7f07000a;
-        public static final int title_activity_settings=0x7f070006;
-        public static final int title_activity_strategy_setting=0x7f070009;
-        public static final int title_activity_tab_page_with_tabs=0x7f070005;
+        public static final int action_settings=0x7f070019;
+        public static final int app_name=0x7f070018;
+        public static final int st_athome_strategy_1=0x7f070008;
+        public static final int st_athome_strategy_2=0x7f070009;
+        public static final int st_athome_strategy_3=0x7f07000a;
+        public static final int st_athome_strategy_4=0x7f07000b;
+        public static final int st_athome_strategy_5=0x7f07000c;
+        public static final int st_athome_strategy_6=0x7f07000d;
+        public static final int st_athome_strategy_7=0x7f07000e;
+        public static final int st_athome_strategy_8=0x7f07000f;
+        public static final int st_awayhome_strategy_1=0x7f070000;
+        public static final int st_awayhome_strategy_2=0x7f070001;
+        public static final int st_awayhome_strategy_3=0x7f070002;
+        public static final int st_awayhome_strategy_4=0x7f070003;
+        public static final int st_awayhome_strategy_5=0x7f070004;
+        public static final int st_awayhome_strategy_6=0x7f070005;
+        public static final int st_awayhome_strategy_7=0x7f070006;
+        public static final int st_awayhome_strategy_8=0x7f070007;
+        public static final int st_revocate_strategy_1=0x7f070010;
+        public static final int st_revocate_strategy_2=0x7f070011;
+        public static final int st_revocate_strategy_3=0x7f070012;
+        public static final int st_revocate_strategy_4=0x7f070013;
+        public static final int st_revocate_strategy_5=0x7f070014;
+        public static final int st_revocate_strategy_6=0x7f070015;
+        public static final int st_revocate_strategy_7=0x7f070016;
+        public static final int st_revocate_strategy_8=0x7f070017;
+        public static final int title_activity_activity1=0x7f07001a;
+        public static final int title_activity_activity2=0x7f07001b;
+        public static final int title_activity_activity3=0x7f07001c;
+        public static final int title_activity_local_setting=0x7f07001f;
+        public static final int title_activity_security_setting=0x7f070020;
+        public static final int title_activity_sensor_setting=0x7f070022;
+        public static final int title_activity_settings=0x7f07001e;
+        public static final int title_activity_strategy_setting=0x7f070021;
+        public static final int title_activity_tab_page_with_tabs=0x7f07001d;
     }
     public static final class style {
         /**  fill parent in vertical & horizontal component 
