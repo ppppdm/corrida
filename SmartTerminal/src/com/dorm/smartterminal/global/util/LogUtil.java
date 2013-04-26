d4b9baaf172f18c0514da808e6b19da1c427f32e
pdm <ppppdm@gmail.com>
2013-04-26 14:16:48 +0800

    add LogUtil log to sdcard

--- SmartTerminal/src/com/dorm/smartterminal/global/util/LogUtil.java
+++ SmartTerminal/src/com/dorm/smartterminal/global/util/LogUtil.java
@@ -1,7 +1,13 @@
 package com.dorm.smartterminal.global.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Random;
 
+import android.os.Environment;
 import android.util.Log;
 
 /**
@@ -19,15 +25,52 @@ public class LogUtil {
      * 
      * @return
      */
+
+    static OutputStreamWriter writer;
+
+    static String LOG_SERVICE_LOG_PATH;
+    
+    static {
+        LOG_SERVICE_LOG_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "log.txt";
+
+        try {
+            writer = new OutputStreamWriter(new FileOutputStream(LOG_SERVICE_LOG_PATH, true));
+        }
+        catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public LogUtil() {
+
+    }
+
     public static void log(String tag, String info) {
 
         Log.i(tag, info);
+        
+        try {
+            writer.write(tag+" "+ info);
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
-    
+
     public static void log(Object caller, String info) {
 
         Log.i(caller.getClass().getSimpleName(), info);
+        
+        try {
+            writer.write(caller.getClass().getSimpleName()+" "+ info);
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
 
